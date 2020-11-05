5d225bf9dfd476c3f6365609e21afda8a6976348
429273-13 <429273-13>
2020-11-05 11:32:17 +0300

    lab3

--- index.php
+++ index.php
@@ -12,5 +12,31 @@ require "Nikitenko/Equation.php";
 require "Nikitenko/QuadEquation.php";
 require "Nikitenko/NikitenkoException.php";
 
- 
+
+ini_set("display_errors", 1);
+error_reporting (-1);
+
+$b = new E();
+try
+{
+    $values = array();
+    for ($i = 0; $i < 3; $i++)
+    {
+        echo "Введите " . ($i+1) . "й-аргумент:";
+        $values[$i] = (float)readline();
+    }
+
+    MyLog::Log("Введено уравнение" . $values[0] . "x^2+" . $values[1] . "x+" . $values[2]);
+
+    $x = $b->solve($values[0], $values[1], $values[2]);
+
+    $str = implode(", ", $x);
+    MyLog::Log("Корни уравнения: " . $str);
+}
+catch (Exception $e)
+{
+    MyLog::Log($e->getMessage());
+}
+MyLog::write();
+
 ?>
